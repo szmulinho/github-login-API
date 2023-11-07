b0ae7141a8226648ac3af3ae731027f4497bafa1
szmulinho <samik1997@gmail.com>
2023-11-07 15:22:51 +0100

    model

--- internal/model/model.go
+++ internal/model/model.go
@@ -5,19 +5,21 @@ import (
 )
 
 type GithubUser struct {
-	ID           int64        `gorm:"unique_index"`
-	Login        string       `json:"login"`
-	AvatarUrl    string       `json:"avatar_url"`
-	HtmlUrl      string       `json:"html_url"`
-	Email        string       `json:"email"`
-	Role         string       `json:"role"`
-	AccessToken  string       `json:"-"`
-	Repositories []Repository `json:"repositories"`
+	ID           int64         `gorm:"unique_index"`
+	Login        string        `json:"username"`
+	AvatarUrl    string        `json:"avatar_url"`
+	HtmlUrl      string        `json:"html_url"`
+	Email        string        `json:"email"`
+	Role         string        `json:"role"`
+	AccessToken  string        `json:"-"`
+	Repositories []*Repository `gorm:"many2many:user_repositories;"`
 }
 
 type Repository struct {
-	Name       string `json:"name"`
-	Permission string `json:"permission"`
+	ID           int64  `gorm:"primaryKey;autoIncrement"`
+	Name         string `json:"name"`
+	Permission   string `json:"permission"`
+	GithubUserID int64  `gorm:"many2many:user_repositories;"`
 }
 
 var JwtKey = []byte(os.Getenv("JWT_KEY"))
