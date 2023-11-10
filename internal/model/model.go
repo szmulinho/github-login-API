a0cf1fc1677da72d38468d2fcce14064e7f069fc
szmulinho <samik1997@gmail.com>
2023-11-10 18:30:10 +0100

    model

--- internal/model/model.go
+++ internal/model/model.go
@@ -1,6 +1,8 @@
 package model
 
 import (
+	"database/sql/driver"
+	"encoding/json"
 	"gorm.io/gorm"
 	"os"
 )
@@ -8,11 +10,14 @@ import (
 type GitHubLogin struct {
 	gorm.Model
 	PublicRepos []PublicRepo `gorm:"foreignKey:GitHubLoginID"`
-	User        struct {
-		Login     string `json:"login"`
-		Email     string `json:"email"`
-		Followers int    `json:"followers"`
-	} `json:"user"`
+	User        User         `gorm:"foreignKey:GitHubLoginID"`
+}
+
+type User struct {
+	gorm.Model
+	Login     string `json:"login"`
+	Email     string `json:"email"`
+	Followers int    `json:"followers"`
 }
 
 type PublicRepo struct {
@@ -23,3 +28,11 @@ type PublicRepo struct {
 }
 
 var JwtKey = []byte(os.Getenv("JWT_KEY"))
+
+func (u *User) Value() (driver.Value, error) {
+	return json.Marshal(u)
+}
+
+func (u *User) Scan(value interface{}) error {
+	return json.Unmarshal(value.([]byte), u)
+}
