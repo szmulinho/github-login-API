4de38c31d1579b17ade6c6ffbbdfd36f9fbcd0ae
szmulinho <samik1997@gmail.com>
2023-11-11 17:46:41 +0100

    public repos json

--- internal/model/model.go
+++ internal/model/model.go
@@ -21,7 +21,7 @@ type GithubUser struct {
 	Followers   int          `json:"followers"`
 	AccessToken string       `json:"-"`
 	Role        string       `json:"role"`
-	PublicRepos []PublicRepo `gorm:"foreignKey:GitHubLoginID"`
+	PublicRepos []PublicRepo `json:"public_repos" gorm:"foreignKey:GitHubLoginID"`
 }
 
 type PublicRepo struct {
