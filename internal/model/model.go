1f37651da9e3813785c9d08f78bbe72010bf138a
szmulinho <samik1997@gmail.com>
2023-11-11 16:19:54 +0100

    model v2

--- internal/model/model.go
+++ internal/model/model.go
@@ -15,20 +15,23 @@ type GitHubLogin struct {
 
 type GithubUser struct {
 	gorm.Model
-	Login       string `json:"login"`
-	Email       string `json:"email"`
-	AvatarUrl   string `json:"avatarUrl"`
-	Followers   int    `json:"followers"`
-	AccessToken string `json:"-"`
-	Role        string `json:"role"`
+	Login       string       `json:"login"`
+	Email       string       `json:"email"`
+	AvatarUrl   string       `json:"avatarUrl"`
+	Followers   int          `json:"followers"`
+	AccessToken string       `json:"-"`
+	Role        string       `json:"role"`
+	PublicRepos []PublicRepo `gorm:"foreignKey:GitHubLoginID"`
 }
 
 type PublicRepo struct {
 	gorm.Model
-	GitHubLoginID uint   `gorm:"foreignKey"`
-	ID            int    `json:"id"`
-	Name          string `json:"name"`
-	Description   string `json:"description"`
+	GitHubLoginID   uint `gorm:"foreignKey"`
+	GithubUser      GithubUser
+	ID              int    `json:"id"`
+	Name            string `json:"name"`
+	Description     string `json:"description"`
+	GithubUserLogin string // Field to store the GithubUser Login
 }
 
 var JwtKey = []byte(os.Getenv("JWT_KEY"))
