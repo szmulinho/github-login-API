5927bf8c2d0c08a2f788e02b90568b1eb3cd7ec2
szmulinho <samik1997@gmail.com>
2023-11-14 21:11:13 +0100

    check

--- internal/server/endpoints/callback.go
+++ internal/server/endpoints/callback.go
@@ -9,6 +9,20 @@ import (
 )
 
 func (h *handlers) HandleCallback(w http.ResponseWriter, r *http.Request) {
+	var githubUser model.GithubUser
+	var publicRepos []model.PublicRepo
+	var publicRepo model.PublicRepo
+
+	tokenString, err := h.GenerateToken(w, r, githubUser, true)
+	if err != nil {
+		log.Println("Error generating token:", err)
+		http.Error(w, "Internal Server Error", http.StatusInternalServerError)
+		return
+	}
+
+	w.Header().Set("Authorization", "Bearer "+tokenString)
+	h.getUserFromToken(tokenString)
+
 	code := r.URL.Query().Get("code")
 
 	token, err := oauthConfig2.Exchange(r.Context(), code)
@@ -34,14 +48,12 @@ func (h *handlers) HandleCallback(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
-	var githubUser model.GithubUser
 	if err := json.Unmarshal([]byte(githubData), &githubUser); err != nil {
 		log.Println("Error parsing GitHub data:", err)
 		http.Error(w, "Internal Server Error", http.StatusInternalServerError)
 		return
 	}
 
-	var publicRepos []model.PublicRepo
 	if err := json.Unmarshal([]byte(reposResp), &publicRepos); err != nil {
 		log.Println("Error parsing GitHub repositories data:", err)
 		http.Error(w, "Internal Server Error", http.StatusInternalServerError)
@@ -80,7 +92,6 @@ func (h *handlers) HandleCallback(w http.ResponseWriter, r *http.Request) {
 		}
 	}
 
-	var publicRepo model.PublicRepo
 	if err := json.Unmarshal([]byte(githubData), &publicRepo); err != nil {
 		log.Println("Error parsing GitHub data:", err)
 		http.Error(w, "Internal Server Error", http.StatusInternalServerError)
