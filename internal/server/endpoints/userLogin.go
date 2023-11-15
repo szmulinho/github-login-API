3f05aebbf5afcc458130ba6cccfdcca53a9a28e1
szmulinho <samik1997@gmail.com>
2023-11-15 21:35:46 +0100

    userLogin

--- internal/server/endpoints/userLogin.go
+++ internal/server/endpoints/userLogin.go
@@ -1,53 +1,52 @@
-package endpoints
-
-import (
-	"encoding/json"
-	"github.com/szmulinho/github-login/internal/model"
-	"net/http"
-)
-
-func (h *handlers) Login(w http.ResponseWriter, r *http.Request) {
-	var credentials struct {
-		Login string `json:"login"`
-	}
-
-	err := json.NewDecoder(r.Body).Decode(&credentials)
-	if err != nil {
-		http.Error(w, err.Error(), http.StatusBadRequest)
-		return
-	}
-
-	var githubUser model.GithubUser
-	result := h.db.Where("login = ?", credentials.Login).First(&githubUser)
-	if result.Error != nil {
-		http.Error(w, "Invalid login", http.StatusUnauthorized)
-		return
-	}
-
-	var isGithubUser bool
-
-	if githubUser.Role == "user" || githubUser.Role == "doctor" {
-		isGithubUser = true
-	}
-
-	token, err := h.GenerateToken(w, r, githubUser.Login, isGithubUser)
-	if err != nil {
-		http.Error(w, err.Error(), http.StatusInternalServerError)
-		return
-	}
-
-	response := model.LoginResponse{
-		GithubUser: githubUser,
-		Token:      token,
-	}
-
-	responseJSON, err := json.Marshal(response)
-	if err != nil {
-		http.Error(w, err.Error(), http.StatusInternalServerError)
-		return
-	}
-
-	w.Header().Set("Content-Type", "application/json")
-	w.WriteHeader(http.StatusOK)
-	w.Write(responseJSON)
-}
+//package endpoints
+//
+//import (
+//	"encoding/json"
+//	"github.com/szmulinho/github-login/internal/model"
+//	"net/http"
+//)
+//
+//func (h *handlers) Login(w http.ResponseWriter, r *http.Request) {
+//	var credentials struct {
+//		Login string `json:"login"`
+//	}
+//
+//	err := json.NewDecoder(r.Body).Decode(&credentials)
+//	if err != nil {
+//		http.Error(w, err.Error(), http.StatusBadRequest)
+//		return
+//	}
+//
+//	var githubUser model.GithubUser
+//	result := h.db.Where("login = ?", credentials.Login).First(&githubUser)
+//	if result.Error != nil {
+//		http.Error(w, "Invalid login", http.StatusUnauthorized)
+//		return
+//	}
+//
+//	var isGithubUser bool
+//
+//	if githubUser.Role == "user" || githubUser.Role == "doctor" {
+//		isGithubUser = true
+//	}
+//
+//	token, err := h.GenerateToken(w, r, githubUser.Login, isGithubUser)
+//	if err != nil {
+//		http.Error(w, err.Error(), http.StatusInternalServerError)
+//		return
+//	}
+//
+//	response := model.LoginResponse{
+//		GithubUser: githubUser,
+//	}
+//
+//	responseJSON, err := json.Marshal(response)
+//	if err != nil {
+//		http.Error(w, err.Error(), http.StatusInternalServerError)
+//		return
+//	}
+//
+//	w.Header().Set("Content-Type", "application/json")
+//	w.WriteHeader(http.StatusOK)
+//	w.Write(responseJSON)
+//}
