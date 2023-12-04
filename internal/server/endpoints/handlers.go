af8521157ea9d57f09de3a80ebd2ac87ff7fb8df
szmulinho <samik1997@gmail.com>
2023-12-04 15:52:42 +0100

    handlers

--- internal/server/endpoints/handlers.go
+++ internal/server/endpoints/handlers.go
@@ -8,7 +8,8 @@ import (
 type Handlers interface {
 	HandleLogin(w http.ResponseWriter, r *http.Request)
 	HandleCallback(w http.ResponseWriter, r *http.Request)
-	GenerateToken(w http.ResponseWriter, r *http.Request, Login string, isUser bool) (string, error)
+	GenerateUserToken(w http.ResponseWriter, r *http.Request, Login string, isUser bool) (string, error)
+	GenerateDoctorToken(w http.ResponseWriter, r *http.Request, Login string, isDoctor bool) (string, error)
 }
 
 type handlers struct {
